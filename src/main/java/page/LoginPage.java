7dc205ba1c1c7e3d19452cbfd866b4d1c0159bb1
yuchubatiuk2 <yuchubatiuk@exadel.com>
2025-09-14 01:57:05 +0300

    Rewrite webelements using FindBy and PageFactory

--- src/main/java/page/LoginPage.java
+++ src/main/java/page/LoginPage.java
@@ -2,26 +2,36 @@ package page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage extends Page {
 
-    private By userName = By.xpath("//input[@id='username']");
-    private By password = By.xpath("//input[@id='password']");
-    private By loginButton = By.xpath("//button[@type='submit']");
+    @FindBy(xpath = "//input[@id='username']")
+    private WebElement userName;
+
+    @FindBy(xpath = "//input[@id='password']")
+    private WebElement password;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement loginButton;
 
     public LoginPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public void fillCredentialsAndClickLogin(String userNameValue, String passwordValue) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(userName));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(password));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(loginButton));
 
-        driver.findElement(userName).sendKeys(userNameValue);
-        driver.findElement(password).sendKeys(passwordValue);
-        driver.findElement(loginButton).click();
+        wait.until(ExpectedConditions.visibilityOf(userName));
+        wait.until(ExpectedConditions.visibilityOf(password));
+        wait.until(ExpectedConditions.visibilityOf(loginButton));
+
+        userName.sendKeys(userNameValue);
+        password.sendKeys(passwordValue);
+        loginButton.click();
     }
 }
--- src/test/java/com/abc/LoginTest.java
+++ src/test/java/com/abc/LoginTest.java
@@ -3,6 +3,8 @@ package com.abc;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
@@ -17,52 +19,29 @@ import static org.testng.Assert.assertEquals;
 
 public class LoginTest extends BaseTest{
 
+    @FindBy(xpath = "//div[@id='flash']")
+    private WebElement greenToaster;
+
+    @FindBy(xpath = "//a[@href='/logout']")
+    private WebElement logoutButton;
+
     @Test
     public void test5() throws InterruptedException {
-
         Logger log = LoggerFactory.getLogger(LoginTest.class);
 
         log.info("Step 1: Open website");
         driver.get("https://the-internet.herokuapp.com/login");
-        Thread.sleep(Long.parseLong("2000")); // added for visibility of page initial state
-
-//        By userName = By.xpath("//input[@id='username']");
-//        By password = By.xpath("//input[@id='password']");
-//        By loginButton = By.xpath("//button[@type='submit']");
-
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(userName));
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(password));
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(loginButton));
-
-//        log.info("Step 2: Enter username");
-//        WebElement userNameElement = driver.findElement(userName);
-//        String userNameValue = "tomsmith";
-//        userNameElement.sendKeys(userNameValue);
-//
-//        log.info("Step 3: Enter password");
-//        WebElement passwordElement = driver.findElement(password);
-//        String passwordValue = "SuperSecretPassword!";
-//        passwordElement.sendKeys(passwordValue);
-//
-//        log.info("Step 4: Click Login button");
-//        WebElement loginButtonElement = driver.findElement(loginButton);
-//        loginButtonElement.click();
 
         log.info("Step 2: Fill in credentials and click login");
         LoginPage loginPage = new LoginPage(driver);
-
         loginPage.fillCredentialsAndClickLogin("tomsmith","SuperSecretPassword!");
 
         log.info("Step 3: Verify Login is successful");
-        By greenToaster = By.xpath("//div[@id='flash']");
-        By logoutButton = By.xpath("//a[@href='/logout']");
+        PageFactory.initElements(driver, this);
+        wait.until(ExpectedConditions.visibilityOf(greenToaster));
+        wait.until(ExpectedConditions.visibilityOf(logoutButton));
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(greenToaster));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(logoutButton));
-
-        WebElement greenToasterText = driver.findElement(greenToaster);
-        WebElement logoutButtonElement = driver.findElement(logoutButton);
-        Assert.assertTrue(greenToasterText.isDisplayed(), "Green toaster with text \"You logged into a secure area!\" should be displayed");
-        Assert.assertTrue(logoutButtonElement.isDisplayed(), "Logout button should be displayed");
+        Assert.assertTrue(greenToaster.isDisplayed(), "Green toaster with text \"You logged into a secure area!\" should be displayed");
+        Assert.assertTrue(logoutButton.isDisplayed(), "Logout button should be displayed");
     }
 }
